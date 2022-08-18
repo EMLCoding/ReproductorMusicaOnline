9a707e2c31097c293e9ea2bcd3d626159918eb97
Eduardo Martin Lorenzo <eduardomartinlorenzo@MacStudeEduardo.home>
2022-08-18 16:45:23 +0200

    Proyecto terminado

---
+++ src/app/pages/player/player.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { AudioService } from 'src/app/services/audio.service';
+import { CloudService } from 'src/app/services/cloud.service';
+import { StreamState } from '../../interfaces/stream-state';
+
+@Component({
+  selector: 'app-player',
+  templateUrl: './player.component.html',
+  styleUrls: ['./player.component.scss']
+})
+export class PlayerComponent {
+
+  constructor(
+    public audioService: AudioService,
+    public cloudService: CloudService
+  ) {
+    // get media files
+    cloudService.getFiles().subscribe(files => {
+      this.files = files;
+    });
+
+    // listen to stream state
+    this.audioService.getState().subscribe(state => {
+      this.state = state;
+    });
+  }
+
+  files: Array<any> = [];
+  state: StreamState | undefined;
+  currentFile: any = {};
+
+  isFirstPlaying() {
+    return this.currentFile.index === 0;
+  }
+
+  isLastPlaying() {
+    return this.currentFile.index === this.files.length - 1;
+  }
+
+  playStream(url: any) {
+    this.audioService.playStream(url).subscribe(events => {
+      // listening for fun here
+    });
+  }
+
+  openFile(file: any, index: any) {
+    this.currentFile = { index, file };
+    this.audioService.stop();
+    this.playStream(file.url);
+  }
+
+  pause() {
+    this.audioService.pause();
+  }
+
+  play() {
+    this.audioService.play();
+  }
+
+  stop() {
+    this.audioService.stop();
+  }
+
+  next() {
+    const index = this.currentFile.index + 1;
+    const file = this.files[index];
+    this.openFile(file, index);
+  }
+
+  previous() {
+    const index = this.currentFile.index - 1;
+    const file = this.files[index];
+    this.openFile(file, index);
+  }
+
+  onSliderChangeEnd(change: any) {
+    this.audioService.seekTo(change.value);
+  }
+
+}
